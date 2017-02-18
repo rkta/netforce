6a6c43c71c061a0baf2d1e20e57e526b2a6b7306
rkta <mail@rkita.de>
2017-02-18 22:06:30 +0100

    Add more COLOROPTIONS for plotting

--- resultant.py
+++ resultant.py
@@ -14,7 +14,132 @@ import re
 import math
 import matplotlib.pyplot as plt
 
-COLOROPTIONS = ['b-', 'g-', 'c-', 'm-', 'y-', 'k-', 'w-']
+COLOROPTIONS = [
+'#0000FF',	# 'blue':
+'#008000',	# 'green':
+'#00FFFF',	# 'cyan':
+'#FF00FF',	# 'magenta':
+'#FFFF00',	# 'yellow':
+'#000000',	# 'black':
+'#A52A2A',	# 'brown':
+'#FF1493',	# 'deeppink':
+'#8A2BE2',	# 'blueviolet':
+'#DEB887',	# 'burlywood':
+'#5F9EA0',	# 'cadetblue':
+'#7FFF00',	# 'chartreuse':
+'#D2691E',	# 'chocolate':
+'#FF7F50',	# 'coral':
+'#6495ED',	# 'cornflowerblue':
+'#00BFFF',	# 'deepskyblue':
+'#696969',	# 'dimgray':
+'#1E90FF',	# 'dodgerblue':
+'#B22222',	# 'firebrick':
+'#228B22',	# 'forestgreen':
+'#FF00FF',	# 'fuchsia':
+'#DCDCDC',	# 'gainsboro':
+'#FFD700',	# 'gold':
+'#DAA520',	# 'goldenrod':
+'#808080',	# 'gray':
+'#ADFF2F',	# 'greenyellow':
+'#F0FFF0',	# 'honeydew':
+'#FF69B4',	# 'hotpink':
+'#CD5C5C',	# 'indianred':
+'#4B0082',	# 'indigo':
+'#FFFFF0',	# 'ivory':
+'#F0E68C',	# 'khaki':
+'#E6E6FA',	# 'lavender':
+'#FFF0F5',	# 'lavenderblush':
+'#7CFC00',	# 'lawngreen':
+'#FFFACD',	# 'lemonchiffon':
+'#ADD8E6',	# 'lightblue':
+'#F08080',	# 'lightcoral':
+'#E0FFFF',	# 'lightcyan':
+'#FAFAD2',	# 'lightgoldenrodyellow':
+'#90EE90',	# 'lightgreen':
+'#D3D3D3',	# 'lightgray':
+'#FFB6C1',	# 'lightpink':
+'#FFA07A',	# 'lightsalmon':
+'#20B2AA',	# 'lightseagreen':
+'#87CEFA',	# 'lightskyblue':
+'#778899',	# 'lightslategray':
+'#B0C4DE',	# 'lightsteelblue':
+'#FFFFE0',	# 'lightyellow':
+'#00FF00',	# 'lime':
+'#32CD32',	# 'limegreen':
+'#FAF0E6',	# 'linen':
+'#800000',	# 'maroon':
+'#66CDAA',	# 'mediumaquamarine':
+'#0000CD',	# 'mediumblue':
+'#BA55D3',	# 'mediumorchid':
+'#9370DB',	# 'mediumpurple':
+'#3CB371',	# 'mediumseagreen':
+'#7B68EE',	# 'mediumslateblue':
+'#00FA9A',	# 'mediumspringgreen':
+'#48D1CC',	# 'mediumturquoise':
+'#C71585',	# 'mediumvioletred':
+'#191970',	# 'midnightblue':
+'#F5FFFA',	# 'mintcream':
+'#FFE4E1',	# 'mistyrose':
+'#FFE4B5',	# 'moccasin':
+'#000080',	# 'navy':
+'#FDF5E6',	# 'oldlace':
+'#808000',	# 'olive':
+'#6B8E23',	# 'olivedrab':
+'#FFA500',	# 'orange':
+'#FF4500',	# 'orangered':
+'#DA70D6',	# 'orchid':
+'#EEE8AA',	# 'palegoldenrod':
+'#98FB98',	# 'palegreen':
+'#AFEEEE',	# 'paleturquoise':
+'#DB7093',	# 'palevioletred':
+'#FFEFD5',	# 'papayawhip':
+'#FFDAB9',	# 'peachpuff':
+'#CD853F',	# 'peru':
+'#FFC0CB',	# 'pink':
+'#DDA0DD',	# 'plum':
+'#B0E0E6',	# 'powderblue':
+'#800080',	# 'purple':
+'#FF0000',	# 'red':
+'#BC8F8F',	# 'rosybrown':
+'#4169E1',	# 'royalblue':
+'#8B4513',	# 'saddlebrown':
+'#FA8072',	# 'salmon':
+'#FAA460',	# 'sandybrown':
+'#2E8B57',	# 'seagreen':
+'#FFF5EE',	# 'seashell':
+'#A0522D',	# 'sienna':
+'#C0C0C0',	# 'silver':
+'#87CEEB',	# 'skyblue':
+'#6A5ACD',	# 'slateblue':
+'#708090',	# 'slategray':
+'#FFFAFA',	# 'snow':
+'#00FF7F',	# 'springgreen':
+'#4682B4',	# 'steelblue':
+'#D2B48C',	# 'tan':
+'#008080',	# 'teal':
+'#D8BFD8',	# 'thistle':
+'#FF6347',	# 'tomato':
+'#40E0D0',	# 'turquoise':
+'#EE82EE',	# 'violet':
+'#F5DEB3',	# 'wheat':
+'#9ACD32',	# 'yellowgreen':
+'#00008B',	# 'darkblue':
+'#008B8B',	# 'darkcyan':
+'#B8860B',	# 'darkgoldenrod':
+'#A9A9A9',	# 'darkgray':
+'#006400',	# 'darkgreen':
+'#BDB76B',	# 'darkkhaki':
+'#8B008B',	# 'darkmagenta':
+'#556B2F',	# 'darkolivegreen':
+'#FF8C00',	# 'darkorange':
+'#9932CC',	# 'darkorchid':
+'#8B0000',	# 'darkred':
+'#E9967A',	# 'darksalmon':
+'#8FBC8F',	# 'darkseagreen':
+'#483D8B',	# 'darkslateblue':
+'#2F4F4F',	# 'darkslategray':
+'#00CED1',	# 'darkturquoise':
+'#9400D3']	# 'darkviolet':
 
 # All values are stored as a list of lists (valuesList).
 # Everything related to Fr goes to its own list (resultantList).
@@ -92,7 +217,6 @@ def resolveForce(Force, radians):
 def getResultant():
     resultant = [0, 0, 0, 0, 0]
     for i in range(len(valuesList)):
-
         # sum
         resultant[Frx] += valuesList[i][Fx]
         resultant[Fry] += valuesList[i][Fy]
@@ -108,13 +232,12 @@ def outputResults(halt = True):
     if halt:
         os.system("clear")
 
-    head = '\ni\t   F\t        Angle\t   Fx\t\t   Fy'
     if len(valuesList) == 0:
         print('\nYou need to enter something!')
         input('\nPress Enter to continue...')
         return None
 
-    print(head)
+    print('\ni\t   F\t        Angle\t   Fx\t\t   Fy')
     print('-' * 70)
     for i in range(len(valuesList)):
         if len(valuesList[i]) == 5:
@@ -150,9 +273,14 @@ def plot():
         print("\nYou need to run 'calc' first!")
         return
     for i in range(len(valuesList)):
+        if i < len(COLOROPTIONS):
+            color = i
+        else:
+            color = i - len(COLOROPTIONS)
+
         plt.plot([0, round(valuesList[i][Fx], 3)],\
                 [0, round(valuesList[i][Fy], 3)],\
-                COLOROPTIONS[i], label='F[%s]' % str(i+1))
+                COLOROPTIONS[color], label='F[%s]' % str(i+1))
 
     plt.plot([0, round(resultantList[Frx], 3)],\
             [0, round(resultantList[Fry], 3)],\
@@ -170,7 +298,11 @@ print('Calculate the resultant')
 valuesList, resultantList, isCalculated = resetAllValues()
 
 # For testing only, don't ship
-valuesList = [[15.8, 82, math.radians(82)], [23.4, 175, math.radians(175)], [12.5, 270, math.radians(270)], [28.75, 340, math.radians(340)]]
+#valuesList = [[15.8, 82, math.radians(82)], [23.4, 175, math.radians(175)], [12.5, 270, math.radians(270)], [28.75, 340, math.radians(340)]]
+valuesList = []
+#for i in range(len(COLOROPTIONS)):
+for i in range(21):
+    valuesList.append([10, 10 * i, math.radians(10 * i)])
 # For testing only, don't ship
 
 while True:
